d74c0bba5146ad5c05a4d4bceb9e2ff5460ad6dc
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-10-23 16:56:44 -0600

    Autoformat

--- boilerplate/main.py
+++ boilerplate/main.py
@@ -1,6 +1,7 @@
 """
 Copyright (C) 2024 Big Ladder Software, LLC. See LICENSE.txt for license information.
 """
+
 import sys
 from pathlib import Path
 import tomllib
@@ -14,7 +15,7 @@ app = typer.Typer()
 
 
 THIS_DIR = Path(__file__).resolve().parent
-DATA_DIR = (THIS_DIR / '..' / 'data').resolve()
+DATA_DIR = (THIS_DIR / ".." / "data").resolve()
 
 
 @app.command()
@@ -32,12 +33,12 @@ def gen(config_path: str, project_type: str):
     if not tgt_dir.exists() or not p_config.exists():
         print("ERROR")
         sys.exit(2)
-    with open(src_dir / 'manifest.toml', 'rb') as fid:
+    with open(src_dir / "manifest.toml", "rb") as fid:
         manifest = tomllib.load(fid)
-    with open(p_config, 'r') as fid:
+    with open(p_config, "r") as fid:
         config_toml = fid.read()
     config = read_config(config_toml, manifest["parameters"])
-    result = generate(src_dir, tgt_dir, manifest, config, dry_run = False)
+    result = generate(src_dir, tgt_dir, manifest, config, dry_run=False)
     for r in result:
         print(f"- {r}")
 
@@ -49,11 +50,10 @@ def config_for(config_path: str, project_type: str):
     """
     typer.echo(f"Generating config.toml for {project_type} at {config_path}")
     p_config = Path(config_path).resolve()
-    p_manifest = DATA_DIR / project_type / 'manifest.toml'
-    with open(p_manifest, 'rb') as fid:
+    p_manifest = DATA_DIR / project_type / "manifest.toml"
+    with open(p_manifest, "rb") as fid:
         manifest = tomllib.load(fid)
     config_str = create_config_toml(manifest)
     p_config.parent.mkdir(parents=True, exist_ok=True)
-    with open(p_config, 'w') as fid:
+    with open(p_config, "w") as fid:
         fid.write(config_str)
-
