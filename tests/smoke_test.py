4c56b2efd759c53e2ae55eaafe9916806f50caf0
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2026-03-30 10:20:30 -0600

    Add minimal smoke_test file.

---
+++ tests/smoke_test.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+assert (Path(__file__).parent.parent / "atheneum_forge" / "languages" / "cpp").exists()
+assert (Path(__file__).parent.parent / "atheneum_forge" / "languages" / "python").exists()
